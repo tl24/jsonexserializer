cc2f6a95090280d4c71e02b5bf28af6645bc09ff
elliott.ted <elliott.ted@5c4d8bf1-3f31-0410-ab14-a3a9b0a89c4c>
2007-06-11 11:27:09 +0000

    Fleshing out Converters a little more

--- JsonExSerializer/JsonExSerializer/TypeConversion/TypeConverterFactory.cs
+++ JsonExSerializer/JsonExSerializer/TypeConversion/TypeConverterFactory.cs
@@ -26,13 +26,32 @@ namespace JsonExSerializer.TypeConversion
 
         public bool HasConverter(MemberInfo member)
         {
-            return _registeredTypes.ContainsKey(member)
-            || (member.IsDefined(typeof(JsonConvertAttribute), false))
-            || member.IsDefined(typeof(TypeConverterAttribute), false);
+            if (_registeredTypes.ContainsKey(member)
+            || (member.IsDefined(typeof(JsonConvertAttribute), false))) {
+                return true;
+            } else {
+                Type t = null;
+                if (member is Type)
+                    t = member as Type;
+                else
+                    return false;
+
+                // don't use converters for primitives and strings
+                if (t.IsPrimitive || t == typeof(string))
+                    return false;
+
+                TypeConverter converter = TypeDescriptor.GetConverter(t);
+                return converter.CanConvertFrom(typeof(string)) && converter.CanConvertTo(typeof(string));
+            }
         }
 
         public IJsonTypeConverter GetConverter(MemberInfo forMember)
         {
+            if (!(forMember is Type || forMember is PropertyInfo))
+            {
+                throw new ArgumentException("forMember parameter to GetConverter must be either a Type, or PropertyInfo");
+            }
+
             if (_registeredTypes.ContainsKey(forMember))
             {
                 return _registeredTypes[forMember];
@@ -42,7 +61,7 @@ namespace JsonExSerializer.TypeConversion
                 // just one for now, but later support chaining of converters
                 JsonConvertAttribute convAttr = (JsonConvertAttribute) forMember.GetCustomAttributes(typeof(JsonConvertAttribute), false)[0];
                 IJsonTypeConverter converter = (IJsonTypeConverter) Activator.CreateInstance(convAttr.Converter);
-                converter.SourceType = forMember is Type ? (Type) forMember : forMember.DeclaringType;
+                converter.SourceType = forMember is Type ? (Type) forMember : ((PropertyInfo) forMember).PropertyType;
                 _registeredTypes[forMember] = converter;
                 // should we register it?
                 return converter;
@@ -50,7 +69,7 @@ namespace JsonExSerializer.TypeConversion
             else
             {
                 // System.ComponentModel.TypeConverter
-                IJsonTypeConverter converter = new TypeConverterAdapter(TypeDescriptor.GetConverter(forMember.DeclaringType));
+                IJsonTypeConverter converter = new TypeConverterAdapter(TypeDescriptor.GetConverter((Type) forMember));
                 _registeredTypes[forMember] = converter;
                 return converter;
             }
--- JsonExSerializer/JsonExSerializerTests/ConversionTests.cs
+++ JsonExSerializer/JsonExSerializerTests/ConversionTests.cs
@@ -4,6 +4,7 @@ using System.Text;
 using NUnit.Framework;
 using JsonExSerializer.TypeConversion;
 using JsonExSerializerTests.Mocks;
+using System.ComponentModel;
 
 namespace JsonExSerializerTests
 {
@@ -17,7 +18,48 @@ namespace JsonExSerializerTests
             TypeConverterFactory factory = new TypeConverterFactory();
             bool f = factory.HasConverter(typeof(Guid));
 
+            Assert.IsTrue(f, "Guid should have implicit TypeConverter through System.ComponentModel framework");
+
             f = factory.HasConverter(typeof(SimpleObject));
+
+            Assert.IsFalse(f, "SimpleObject should not have converter");
+
+            
+            f = factory.HasConverter(typeof(SimpleObject).GetProperty("ByteValue"));
+
+            Assert.IsFalse(f, "SimpleObject.ByteValue property does not have a converter");
+
+            // test an unexpected type
+            f = factory.HasConverter(typeof(SimpleObject).GetConstructor(new Type[0]));
+            Assert.IsFalse(f, "Can't test for converter from a constructor");
+
+            // test primitives
+            f = factory.HasConverter(typeof(int));
+            Assert.IsFalse(f, "No converters for primitive types");
+
+            f = factory.HasConverter(typeof(string));
+            Assert.IsFalse(f, "No converters for string");
+        }
+
+        [Test]
+        public void ConvertGuidTest()
+        {
+            TypeConverterFactory factory = new TypeConverterFactory();
+            if (factory.HasConverter(typeof(Guid)))
+            {
+                IJsonTypeConverter converter = factory.GetConverter(typeof(Guid));
+                Guid g = Guid.NewGuid();
+                object o = converter.ConvertFrom(g);
+                Assert.IsTrue(o is string, "Converter should convert guid to string");
+                Guid result = (Guid) converter.ConvertTo(o);
+
+                Assert.AreEqual(g, result, "Guid did not convert correctly");
+            }
+            else
+            {
+                Assert.Fail("Guid should have a typeconverter");
+            }
+
         }
     }
 }

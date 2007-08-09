af0e63f390af4a5fd748b4f77cda4eff8462097c
elliott.ted <elliott.ted@5c4d8bf1-3f31-0410-ab14-a3a9b0a89c4c>
2007-08-09 05:27:53 +0000

    Release 1.0

--- JsonExSerializer/JsonExSerializer/Properties/AssemblyInfo.cs
+++ JsonExSerializer/JsonExSerializer/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("12d697c6-ebf8-479f-b78a-86fdf09307d0")]
 [assembly: AssemblyInformationalVersion("1.0")]
-[assembly: AssemblyVersion("1.0.0.61")]
-[assembly: AssemblyFileVersion("1.0.0.61")]
+[assembly: AssemblyVersion("1.0.0.62")]
+[assembly: AssemblyFileVersion("1.0.0.62")]
 
 
